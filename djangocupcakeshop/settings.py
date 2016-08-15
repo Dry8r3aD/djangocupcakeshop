e8dc26e9846ef20aae6535d3850d5fd9bcf7c7c9
Dry8r3aD <me@dry8r3ad.com>
2016-08-15 08:58:10 +0000

    done

--- djangocupcakeshop/settings.py
+++ djangocupcakeshop/settings.py
@@ -16,16 +16,25 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+DEBUG = os.getenv('DJANGO_DEBUG') != 'FALSE'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1dp)-1!_ghogb^g-pqg@m25&k&fjemp*#c20-wc*)oaz8t@6b5'
+if DEBUG:
+    SECRET_KEY = 'Hell@World!'
+else:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+if DEBUG:
+    ALLOWED_HOSTS = ['*']
+else:
+    ALLOWED_HOSTS = ['dry8r3ad.pythonanywhere.com']
 
 
 # Application definition
